952d5884483b8b1e55d323788db2cc97a2343616
javin23863 <joshuajacob2386@gmail.com>
2026-08-30 10:40:49 +0700

    Export production execution specifications

--- product/tradercockpit/domain/__init__.py
+++ product/tradercockpit/domain/__init__.py
@@ -7,10 +7,28 @@ from .canonical import (
     canonical_sha256,
     content_address,
 )
+from .specs import (
+    BacktestRunSpecV1,
+    CandidateSpecV1,
+    DataSpecV1,
+    EngineBuildSpecV1,
+    ExecutionModelV1,
+    ExecutionSpecV1,
+    SpecValidationError,
+    StrategySpecV1,
+)
 
 __all__ = [
+    "BacktestRunSpecV1",
+    "CandidateSpecV1",
     "CanonicalizationError",
     "ContentAddress",
+    "DataSpecV1",
+    "EngineBuildSpecV1",
+    "ExecutionModelV1",
+    "ExecutionSpecV1",
+    "SpecValidationError",
+    "StrategySpecV1",
     "canonical_json_bytes",
     "canonical_sha256",
     "content_address",
