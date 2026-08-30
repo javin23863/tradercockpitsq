a9f653168c5652e90d93b49c0388f22abc098c69
javin23863 <joshuajacob2386@gmail.com>
2026-08-30 10:46:35 +0700

    Export validated evaluator contract

--- product/tradercockpit/engine/__init__.py
+++ product/tradercockpit/engine/__init__.py
@@ -6,10 +6,18 @@ from .contracts import (
     SpecResolver,
     resolve_backtest_inputs,
 )
+from .evaluator import (
+    BacktestEvaluatorV1,
+    EvaluatorDescriptorV1,
+    evaluate_backtest,
+)
 
 __all__ = [
+    "BacktestEvaluatorV1",
     "BacktestInputsV1",
     "EngineContractError",
+    "EvaluatorDescriptorV1",
     "SpecResolver",
+    "evaluate_backtest",
     "resolve_backtest_inputs",
 ]
